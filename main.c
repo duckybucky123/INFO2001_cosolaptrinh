1bd4a9f0b871d77ead8c7177d0db5d3df5744ad8
duckybucky123 <laminhduc005@gmail.com>
2023-11-06 16:24:10 +0700

    chinh sua main.c

--- main.c
+++ main.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
+
+#define GIA_TIEN_TRUOC_12H 6000
+#define GIA_TIEN_SAU_12H 7500
+
 int main() {
-    int a, b;
-    printf("Nhap vao hai so nguyen a va b: ");
-    scanf("%d %d", &a, &b);
+    int gioVaoCa, gioRaCa;
+    int tongGioLam, tienLuong;
 
-    if (b != 0) {
-       int quotient = a / b;
-        int remainder = a % b;
+    printf("Gio vao ca: ");
+    scanf("%d", &gioVaoCa);
 
-        printf("Phan nguyen: %d\n", quotient);
-       printf("Phan du: %d\n", remainder);
+    printf("Gio ra ca: ");
+    scanf("%d", &gioRaCa);
+
+    // Xử lý tính số giờ làm
+    tongGioLam = gioRaCa - gioVaoCa;
+
+    // Xử lý tính tiền lương
+    if (gioVaoCa < 12 && gioRaCa <= 12) {
+        tienLuong = tongGioLam * GIA_TIEN_TRUOC_12H;
+    } else if (gioVaoCa < 12 && gioRaCa > 12) {
+        int gioLamTruoc12h = 12 - gioVaoCa;
+        int gioLamSau12h = gioRaCa - 12;
+        tienLuong = (gioLamTruoc12h * GIA_TIEN_TRUOC_12H) + (gioLamSau12h * GIA_TIEN_SAU_12H);
+    } else if (gioVaoCa >= 12 && gioRaCa > 12) {
+        tienLuong = tongGioLam * GIA_TIEN_SAU_12H;
     } else {
-        printf("Loi: b phai khac 0.\n");
+        tienLuong = 0;
     }
+
+    printf("Tien luong ngay la: %d VND\n", tienLuong);
 
     return 0;
 }
