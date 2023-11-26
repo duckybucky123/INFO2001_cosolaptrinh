cd41bab9901b11f19ed1f184088e5fa0b0f6149e
duckybucky123 <laminhduc005@gmail.com>
2023-11-26 23:31:25 +0700

    chinh sua file mian.c va hw01.c them hw struct.c

--- HW01.c
+++ HW01.c
@@ -1,15 +1,62 @@
-//#include<stdio.h>
-//int main() {
-//    int p, q;
-//     char *x;
-//    printf("nhap so p: ");
-//    scanf("%d", &p);
-//    printf("nhap so q: ");
-//    scanf("%d", &q);
-//    x=("(-q/2+(p^3/27 +q^2/2)^1/2)^1/3) +(-q/2-(p^3/27+q^2/4)^1/2)^1/3;");
-//    printf("ket qua bieu thuc la:%d",x);
+//#include <stdio.h>
 //
+//struct Student {
+//    char name[50];
+//    int id;
+//    float score;
+//};
+//
+//void inputStudent(struct Student *s) {
+//    printf("Enter name, ID, and score: ");
+//    scanf("%s %d %f", s->name, &s->id, &s->score);
+//}
+//
+//void displayStudent(struct Student s) {
+//    printf("Name: %s\nID: %d\nScore: %.2f\n", s.name, s.id, s.score);
+//}
+//
+//void updateStudent(struct Student *s) {
+//    printf("Update name and score: ");
+//    scanf("%s %f", s->name, &s->score);
+//}
+//
+//int main() {
+//    struct Student students[100];
+//    int n = 0;
+//
+//    int choice;
+//    do {
+//        printf("\nMenu:\n1. Add student\n2. Display students\n3. Update student\n0. Exit\n");
+//        printf("Enter your choice: ");
+//        scanf("%d", &choice);
+//
+//        switch (choice) {
+//            case 1:
+//                inputStudent(&students[n++]);
+//                break;
+//            case 2:
+//                for (int i = 0; i < n; i++)
+//                    displayStudent(students[i]);
+//                break;
+//            case 3:
+//                printf("Enter student ID to update: ");
+//                int updateId;
+//                scanf("%d", &updateId);
+//                for (int i = 0; i < n; i++) {
+//                    if (students[i].id == updateId) {
+//                        updateStudent(&students[i]);
+//                        printf("Student updated successfully.\n");
+//                        break;
+//                    }
+//                }
+//                break;
+//            case 0:
+//                printf("Exiting program.\n");
+//                break;
+//            default:
+//                printf("Invalid choice. Please try again.\n");
+//        }
+//    } while (choice != 0);
 //
 //    return 0;
-//
 //}
--- main.c
+++ main.c
@@ -1,52 +1,107 @@
-#include <stdio.h>
-
-struct ThoiGian {
-    int gio;
-    int phut;
-    int giay;
-};
-
-int main() {
-    struct ThoiGian tg1, tg2, khoangCach;
-
-
-    printf("Nhap vao moc thoi gian thu nhat:\n");
-    printf("Gio: ");
-    scanf("%d", &tg1.gio);
-    printf("Phut: ");
-    scanf("%d", &tg1.phut);
-    printf("Giay: ");
-    scanf("%d", &tg1.giay);
-
-
-    printf("Nhap vao moc thoi gian thu hai:\n");
-    printf("Gio: ");
-    scanf("%d", &tg2.gio);
-    printf("Phut: ");
-    scanf("%d", &tg2.phut);
-    printf("Giay: ");
-    scanf("%d", &tg2.giay);
-
-
-    khoangCach.gio = tg2.gio - tg1.gio;
-    khoangCach.phut = tg2.phut - tg1.phut;
-    khoangCach.giay = tg2.giay - tg1.giay;
-
-
-    if (khoangCach.giay < 0) {
-        khoangCach.giay += 60;
-        khoangCach.phut--;
-    }
-    if (khoangCach.phut < 0) {
-        khoangCach.phut += 60;
-        khoangCach.gio--;
-    }
-
-
-    printf("\nKhoang cach giua hai diem:\n");
-    printf("Gio: %d\n", khoangCach.gio);
-    printf("Phut: %d\n", khoangCach.phut);
-    printf("Giay: %d\n", khoangCach.giay);
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <time.h>
+//
+//struct Student {
+//    char name[50];
+//    int id;
+//    int age;
+//    float grade;
+//};
+//
+//void bubbleSort(struct Student arr[], int n) {
+//    // Bubble sort implementation
+//    // ...
+//}
+//
+//void selectionSort(struct Student arr[], int n) {
+//    // Selection sort implementation
+//    // ...
+//}
+//
+//int linearSearch(struct Student arr[], int n, float x) {
+//    // Linear search implementation
+//    // ...
+//}
+//
+//int binarySearch(struct Student arr[], int l, int r, float x) {
+//    // Binary search implementation
+//    // ...
+//}
+//
+//int main() {
+//    const int MAX_STUDENTS = 50;
+//    struct Student students[MAX_STUDENTS];
+//
+//    // Input student information
+//    printf("Nhap thong tin sinh vien:\n");
+//    for (int i = 0; i < MAX_STUDENTS; i++) {
+//        printf("Nhap ten: ");
+//        fgets(students[i].name, sizeof(students[i].name), stdin);
+//        printf("Nhap ma so: ");
+//        scanf("%d", &students[i].id);
+//        printf("Nhap tuoi: ");
+//        scanf("%d", &students[i].age);
+//        printf("Nhap diem: ");
+//        scanf("%f", &students[i].grade);
+//        getchar(); // Consume the newline character left in the buffer
+//    }
+//
+//    // Sorting
+//    clock_t start_bubble = clock();
+//    bubbleSort(students, MAX_STUDENTS);
+//    clock_t end_bubble = clock();
+//    double time_bubble = ((double) (end_bubble - start_bubble)) / CLOCKS_PER_SEC;
+//
+//    clock_t start_selection = clock();
+//    selectionSort(students, MAX_STUDENTS);
+//    clock_t end_selection = clock();
+//    double time_selection = ((double) (end_selection - start_selection)) / CLOCKS_PER_SEC;
+//
+//    // Display sorted array
+//    printf("\nSorted Array (Bubble Sort):\n");
+//    for (int i = 0; i < MAX_STUDENTS; i++) {
+//        // Display student information
+//        // ...
+//    }
+//
+//    printf("\nSorted Array (Selection Sort):\n");
+//    for (int i = 0; i < MAX_STUDENTS; i++) {
+//        // Display student information
+//        // ...
+//    }
+//
+//    // Display execution time
+//    printf("\nTime taken by Bubble Sort: %lf seconds\n", time_bubble);
+//    printf("Time taken by Selection Sort: %lf seconds\n", time_selection);
+//
+//    // Search
+//    float searchValue;
+//    printf("\nEnter a grade to search: ");
+//    scanf("%f", &searchValue);
+//
+//    int linearIndex = linearSearch(students, MAX_STUDENTS, searchValue);
+//    if (linearIndex != -1) {
+//        printf("Linear Search: Student found at index %d\n", linearIndex);
+//    } else {
+//        printf("Linear Search: Student not found\n");
+//    }
+//
+//    selectionSort(students, MAX_STUDENTS); // Sorting before binary search
+//    int binaryIndex = binarySearch(students, 0, MAX_STUDENTS - 1, searchValue);
+//    if (binaryIndex != -1) {
+//        printf("Binary Search: Student found at index %d\n", binaryIndex);
+//    } else {
+//        printf("Binary Search: Student not found\n");
+//    }
+//
+//    // Calculate and print the average grade
+//    float sum = 0;
+//    for (int i = 0; i < MAX_STUDENTS; i++) {
+//        sum += students[i].grade;
+//    }
+//    float average = sum / MAX_STUDENTS;
+//
+//    printf("\nDiem trung binh cua lop: %.2f\n", average);
+//
+//    return 0;
+//}
